57b7c335d44460ce24c285b374a19e38cb0e37ef
galaxygoldfish <sebastianhriscu25@gmail.com>
2025-09-08 15:57:03 -0700

    navbar and theme resources

--- composeApp/src/wasmJsMain/kotlin/com/skira/app/App.kt
+++ composeApp/src/wasmJsMain/kotlin/com/skira/app/App.kt
@@ -4,44 +4,57 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 
 import skira.composeapp.generated.resources.Res
 import skira.composeapp.generated.resources.compose_multiplatform
+import skira.composeapp.generated.resources.skira_logo
 
 @Composable
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
+    SKiRATheme {
+        var showWelcomeDialog by remember { mutableStateOf(false) }
+        Scaffold(
+            topBar = {
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                        .height(75.dp)
+                        .background(MaterialTheme.colorScheme.primary),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                    Image(
+                        painter = painterResource(Res.drawable.skira_logo),
+                        contentDescription = null,
+                        modifier = Modifier.padding(start = 20.dp)
+                            .size(45.dp)
+                    )
+                    Text(
+                        text = "SKiRA",
+                        style = MaterialTheme.typography.headlineLarge,
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                            .padding(start = 20.dp),
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
                 }
             }
+        ) {
+
         }
     }
 }
---
+++ composeApp/src/wasmJsMain/kotlin/com/skira/app/Theme.kt
@@ -0,0 +1,75 @@
+package com.skira.app
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.Font
+import skira.composeapp.generated.resources.Res
+import skira.composeapp.generated.resources.geist_regular
+import skira.composeapp.generated.resources.plex_light
+import skira.composeapp.generated.resources.plex_medium
+import skira.composeapp.generated.resources.plex_regular
+
+@Composable
+fun SKiRATheme(content: @Composable () -> Unit) {
+    val IBMPlexSans = FontFamily(
+        Font(Res.font.plex_regular, FontWeight.Normal),
+        Font(Res.font.plex_medium, FontWeight.Medium),
+        Font(Res.font.plex_light, FontWeight.Light)
+    )
+    val Geist = FontFamily(
+        Font(Res.font.geist_regular, FontWeight.Normal)
+    )
+    MaterialTheme(
+        typography = Typography(
+            headlineLarge = TextStyle(
+                fontFamily = Geist,
+                fontWeight = FontWeight.Normal,
+                fontSize = 22.sp,
+                letterSpacing = (-1).sp
+            ),
+            headlineMedium = TextStyle(
+                fontFamily = IBMPlexSans,
+                fontWeight = FontWeight.Normal,
+                fontSize = 15.sp
+            ),
+            headlineSmall = TextStyle(
+                fontFamily = Geist,
+                fontWeight = FontWeight.Light,
+                fontSize = 15.sp
+            ),
+            labelLarge = TextStyle(
+                fontFamily = Geist,
+                fontWeight = FontWeight.Normal,
+                fontSize = 12.sp,
+                letterSpacing = (-2).sp
+            ),
+            labelSmall = TextStyle(
+                fontFamily = IBMPlexSans,
+                fontWeight = FontWeight.Normal,
+                fontSize = 11.sp
+            ),
+            bodyLarge = TextStyle(
+                fontFamily = IBMPlexSans,
+                fontWeight = FontWeight.Normal,
+                fontSize = 10.sp,
+                letterSpacing = (-2).sp
+            )
+        ),
+        colorScheme = lightColorScheme(
+            primary = Color(0XFFE8EFF5),
+            outline = Color(0XFF385D8C),
+            onBackground = Color(0xFF000000)
+        )
+    ) {
+        content()
+    }
+}
